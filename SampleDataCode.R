c3ea8063337bf15e34450b136f861fb0fde682b0
Nima <Nimataghidoust@gmail.com>
2020-05-04 19:07:47 +0430

    All sample files added

--- SampleDataCode.R
+++ SampleDataCode.R
@@ -1,18 +1,51 @@
-setwd("C:/Users/n.taghidoost/Downloads/Compressed/final/en_US")
-con <- file("en_US.twitter.txt", "r")
+setwd("N:/TempThings/final/en_US")
 
-n <- length(count.fields(con, sep = ","))
+conTwitter <- file("en_US.twitter.txt", "r")
+nTwitter <- length(count.fields(conTwitter, sep = ","))
+close(conTwitter)
 
-close(con)
+
+conTwitter <- file("en_US.twitter.txt", "r")
+Twitter <- readLines(conTwitter, n=nTwitter, skipNul=TRUE, warn=FALSE)
+close(conTwitter)
 
 
-con <- file("en_US.twitter.txt", "r")
+InLines <- which(rbinom(nTwitter,1,prob=0.1)==1)
+SampleTwitter <- Twitter[InLines]
 
-InLines <- which(rbinom(n,1,prob=0.1)==1)
+write.table(SampleTwitter, "TrainTwitter.txt", append = FALSE, sep = " ", dec = ".",
+            row.names = TRUE, col.names = TRUE)
+#--------------------------------------------------------------------------------
 
-x <- read.table(con, sep="\n", header=FALSE , quote="" )
+conNews <- file("en_US.news", "r")
+nNews <- length(count.fields(conNews, sep = ","))
+close(conNews)
 
-SampleFile <- x[InLines,]
 
-write.table(SampleFile, "TrainFile.txt", append = FALSE, sep = " ", dec = ".",
+conNews <- file("en_US.news", "r")
+News <- readLines(conNews, n=nNews, skipNul=TRUE, warn=FALSE)
+close(conNews)
+
+
+InLines <- which(rbinom(nNews,1,prob=0.1)==1)
+SampleNews <- News[InLines]
+
+write.table(SampleNews, "TrainNews.txt", append = FALSE, sep = " ", dec = ".",
+            row.names = TRUE, col.names = TRUE)
+#---------------------------------------------------------
+
+conBlogs <- file("en_US.blogs.txt", "r")
+nBlogs <- length(count.fields(conBlogs, sep = ","))
+close(conBlogs)
+
+
+conBlogs <- file("en_US.blogs.txt", "r")
+Blogs <- readLines(conBlogs, n=nBlogs, skipNul=TRUE, warn=FALSE)
+close(conBlogs)
+
+
+InLines <- which(rbinom(nBlogs,1,prob=0.1)==1)
+SampleBlogs <- Blogs[InLines]
+
+write.table(SampleBlogs, "TrainBlogs.txt", append = FALSE, sep = " ", dec = ".",
             row.names = TRUE, col.names = TRUE)
