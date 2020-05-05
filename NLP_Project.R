0fe1cc85d23fc6f9d67020a083067104f14e3fee
Nima <nimataghidoost@gmail.com>
2020-05-05 18:36:52 +0430

    Profanity and Frequency added

--- NLP_Project.R
+++ NLP_Project.R
@@ -1,10 +1,25 @@
 library(quanteda)
-
-setwd("N:/R Courses/CapstoneProject_JHK")
+library(dplyr)
+setwd("C:/Users/n.taghidoost/Desktop/RFunctions/CapstoneProject_JHK")
 
 TrainTwitter <- read.table("TrainTwitter.txt")
-ProfanityWords <- read.table("Profanity.txt",sep="\n")
+ProfanityWords <- read.table("Profanity.txt",sep="\n",quote = "")
 CorpTwitter <- corpus(as.character(TrainTwitter$x))
-  TokTwitterProfane <- tokens(CorpTwitter)
+  TokTwitterProfane <- tokens(CorpTwitter,remove_punct = TRUE, remove_numbers = TRUE, remove_symbols = TRUE)
   
-TokTwitter <- tokens_select(TokTwitter, ProfanityWords, selection = "remove", padding = FALSE)
+TokTwitter <- tokens_select(TokTwitterProfane,c(stopwords("en"),ProfanityWords$V1),
+                            selection = "remove", padding = FALSE)
+
+#a <- cbind(as.character(TrainTwitter[grep("fuck",TrainTwitter$x),]),grep("fuck",TrainTwitter$x))
+
+
+
+-------------------------------------------------
+
+FreqTok <- as.data.frame(table(as.character(TokTwitter)) )
+
+FreqTok <- arrange(FreqTok,desc(FreqTok$Freq))
+
+#60% have 1 freq!
+mean(FreqTok$Freq==1)
+
