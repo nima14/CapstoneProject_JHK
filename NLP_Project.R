a16ed40a24d0bc21aa8ab69fe0853384247903b9
Nima <Nimataghidoust@gmail.com>
2020-05-08 15:44:47 +0430

    RMD added

--- NLP_Project.R
+++ NLP_Project.R
@@ -2,19 +2,24 @@ library(quanteda)
 library(dplyr)
 library(ggplot2)
 library(read)
-
+library(stringi)
+library(stringr)
 Path <- dirname(rstudioapi::getSourceEditorContext()$path)
 setwd(Path)
 
 TrainTwitter <- read.table("TrainTwitter.txt",header = TRUE,fill=TRUE)
 #Delete unkown characters.
-TrainTwitter <- iconv(TrainTwitter, "UTF-8", "UTF-8",sub='') 
+TrainTwitter$x <- iconv(TrainTwitter$x, "UTF-8", "UTF-8",sub='') 
+TrainTwitter$x <- str_replace_all(TrainTwitter$x, "[\r\n]" , " ")
+
 ProfanityWords <- read.table("Profanity.txt",sep="\n",quote = "")
 CorpTwitter <- corpus(as.character(TrainTwitter$x))
 TokTwitter <- tokens(CorpTwitter,remove_punct = TRUE, remove_numbers = TRUE, remove_symbols = TRUE)
-  
+TokTwitter <- tokens_tolower(TokTwitter)  
 TokTwitter <- tokens_select(TokTwitter,c(stopwords("en"),ProfanityWords$V1),
                             selection = "remove", padding = FALSE)
+
+TokTwitter <- tokens_wordstem(TokTwitter)
 
 
 
@@ -24,7 +29,7 @@ FreqTok <- as.data.frame(table(as.character(TokTwitter)) )
 
 FreqTok <- arrange(FreqTok,desc(FreqTok$Freq))
 
-#60% have 1 freq!
+#61% have 1 freq!
 mean(FreqTok$Freq==1)
 
 -------------------------------------------------------
@@ -47,14 +52,49 @@ FreqTokThreeGram <- arrange(FreqTokThreeGram,desc(FreqTokThreeGram$Freq))
 
 -------------------------------
   
+FreqTokThreeGram %>% top_n(20) %>% as.data.frame() %>%
+    ggplot( aes(reorder(Var1,-Freq), Freq))+ geom_col()+
+  theme(axis.text.x=element_text(angle = -90, hjust = 0))+ 
+        labs(x = "3-Gram",y="Frequency")
+
+
 FreqTokTwoGram %>% top_n(20) %>% as.data.frame() %>%
-  ggplot( aes(Var1, Freq))+ geom_col()
+  ggplot( aes(reorder(Var1,-Freq), Freq))+ geom_col()+
+  theme(axis.text.x=element_text(angle = -90, hjust = 0))+ 
+  labs(x = "2-Gram",y="Frequency")
+  
 
-qplot(Threegram, geom="histogram") 
 
-ggplot(df, aes(x, y)) + geom_point()
 
 -----------------------------------------
   
-x <- dfm(TokTwitter,stem=TRUE,tolower = TRUE)
-topfeatures(x,20)
+  
+#Total number of words
+sum(sapply(strsplit(TrainTwitter$x[c(4,5)], " "), length))
+
+stri_stats_latex(TrainTwitter$x)[[4]]
+
+#Mean of number of words in each record
+mean(sapply(strsplit(TrainTwitter$x, " "), length))
+
+stri_stats_latex(TrainTwitter$x)[[4]]/
+  stri_stats_general(TrainTwitter$x)[[1]]
+
+
+#Total number of characters
+sum(sapply(strsplit(TrainTwitter$x, ""), length))
+
+stri_stats_latex(TrainTwitter$x)[[1]]
+#Mean of number of characters in each record.
+mean(sapply(strsplit(TrainTwitter$x, ""), length))
+
+
+stri_stats_latex(TrainTwitter$x)[[1]]/
+  stri_stats_general(TrainTwitter$x)[[1]]
+
+
+
+
+
+
+
--- SampleDataCode.R
+++ SampleDataCode.R
@@ -1,8 +1,31 @@
+library(quanteda)
+library(dplyr)
+library(ggplot2)
+library(read)
+library(stringi)
+library(stringr)
+
+
+
 setwd("N:/TempThings/final/en_US")
 
 conTwitter <- file("en_US.twitter.txt", "r")
 nTwitter <- length(count.fields(conTwitter, sep = ","))
 close(conTwitter)
+
+conNews <- file("en_US.news.txt", "r")
+nNews <- length(count.fields(conNews, sep = ","))
+close(conNews)
+
+conBlogs <- file("en_US.blogs.txt", "r")
+nBlogs <- length(count.fields(conBlogs, sep = ","))
+close(conBlogs)
+
+
+
+if (!file.exists("TrainTwitter.txt"))
+{
+setwd("N:/TempThings/final/en_US")
 
 
 conTwitter <- file("en_US.twitter.txt", "r")
@@ -15,13 +38,13 @@ SampleTwitter <- Twitter[InLines]
 
 write.table(SampleTwitter, "TrainTwitter.txt", append = FALSE, sep = " ", dec = ".",
             row.names = TRUE, col.names = TRUE)
+}
 #--------------------------------------------------------------------------------
+setwd(Path)
 
-conNews <- file("en_US.news", "r")
-nNews <- length(count.fields(conNews, sep = ","))
-close(conNews)
-
-
+if (!file.exists("TrainNews.txt"))
+{
+  
 conNews <- file("en_US.news", "r")
 News <- readLines(conNews, n=nNews, skipNul=TRUE, warn=FALSE)
 close(conNews)
@@ -32,16 +55,13 @@ SampleNews <- News[InLines]
 
 write.table(SampleNews, "TrainNews.txt", append = FALSE, sep = " ", dec = ".",
             row.names = TRUE, col.names = TRUE)
+}
 #---------------------------------------------------------
+setwd(Path)
 
-conBlogs <- file("en_US.blogs.txt", "r")
-nBlogs <- length(count.fields(conBlogs, sep = ","))
-close(conBlogs)
+if (!file.exists("TrainBlogs.txt"))
+{
 
-
-conBlogs <- file("en_US.blogs.txt", "r")
-Blogs <- readLines(conBlogs, n=nBlogs, skipNul=TRUE, warn=FALSE)
-close(conBlogs)
 
 
 InLines <- which(rbinom(nBlogs,1,prob=0.1)==1)
@@ -49,4 +69,7 @@ SampleBlogs <- Blogs[InLines]
 
 write.table(SampleBlogs, "TrainBlogs.txt", append = FALSE, sep = " ", dec = ".",
             row.names = TRUE, col.names = TRUE)
+}
 
+Path <- dirname(rstudioapi::getSourceEditorContext()$path)
+setwd(Path)
