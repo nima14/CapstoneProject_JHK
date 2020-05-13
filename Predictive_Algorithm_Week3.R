94c25d04f39c3e3f840a91b3344ab5ae1733ebf8
Nima <nimataghidoost@gmail.com>
2020-05-13 18:28:36 +0430

    Nothing

--- Predictive_Algorithm_Week3.R
+++ Predictive_Algorithm_Week3.R
@@ -123,5 +123,9 @@ MakeNGrams <- function(Sentence)
   
 }
 
+MakeNGrams(Input)
 
-match(x,Base_FourGram$V1)
+Base_FiveGram[match(x[4],word(Base_FiveGram$V1,1,4))]
+Base_FourGram[match(x[3],word(Base_FourGram$V1,1,3))]
+Base_ThreeGram[match(x[2],word(Base_ThreeGram$V1,1,2))]
+Base_TwoGram[match(x[1],word(Base_TwoGram$V1,1,1))]
