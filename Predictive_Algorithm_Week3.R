7f34d7d42aa6512d4b828a1537369e925a031dcd
Nima <Nimataghidoust@gmail.com>
2020-05-14 15:08:07 +0430

    Main changes. OneG_Words & ... were built

--- Predictive_Algorithm_Week3.R
+++ Predictive_Algorithm_Week3.R
@@ -75,33 +75,53 @@ names(Tok) <- c("x","Source")
 ## Cleaning the Tokens
 
 FreqTok <- data.table( table(as.character(Tok$x))  )
-Base_Tok <- data.table(FreqTok[FreqTok$N>1])
+OneG_Words <- data.table(FreqTok[FreqTok$N>1])
+
 rm(FreqTok)
 
 FreqTokTwoGram <- data.table( table(as.character(Twogram$V1))  )
 Base_TwoGram <- data.table(FreqTokTwoGram[FreqTokTwoGram$N>1])
-rm(FreqTokTwoGram)
+TwoG_Words <- data.table(str_split_fixed(Base_TwoGram$V1," ", 2),Base_TwoGram$N)
+names(TwoG_Words) <- c("V1","V2","N")
+rm(FreqTokTwoGram,Base_TwoGram)
 
 
 FreqTokThreeGram <- data.table( table(as.character(Threegram$V1))  )
 Base_ThreeGram <- data.table(FreqTokThreeGram[FreqTokThreeGram$N>1])
-rm(FreqTokThreeGram)
+ThreeG_Words <- data.table(str_split_fixed(Base_ThreeGram$V1," ", 3),Base_ThreeGram$N)
+names(ThreeG_Words) <- c("V1","V2","V3","N")
+rm(FreqTokThreeGram,Base_ThreeGram)
 
 
 FreqTokFourGram <- data.table( table(as.character(Fourgram$V1))  )
 Base_FourGram <- data.table(FreqTokFourGram[FreqTokFourGram$N>1])
-rm(FreqTokFourGram)
+FourG_Words <- data.table(str_split_fixed(Base_FourGram$V1," ", 4),Base_FourGram$N)
+names(FourG_Words) <- c("V1","V2","V3","V4","N")
+rm(FreqTokFourGram,Base_FourGram)
 
 
 FreqTokFiveGram <- data.table( table(as.character(Fivegram$V1))  )
 Base_FiveGram <- data.table(FreqTokFiveGram[FreqTokFiveGram$N>1])
-rm(FreqTokFiveGram)
+FiveG_Words <- data.table(str_split_fixed(Base_FiveGram$V1," ", 5),Base_FiveGram$N)
+names(FiveG_Words) <- c("V1","V2","V3","V4","V5","N")
+rm(FreqTokFiveGram,Base_FiveGram)
+
+
+
+# Save the files
+
+saveRDS(OneG_Words,"OneG_Words.Rdata")
+saveRDS(TwoG_Words,"TwoG_Words.Rdata")
+saveRDS(ThreeG_Words,"ThreeG_Words.Rdata")
+saveRDS(FourG_Words,"FourG_Words.Rdata")
+saveRDS(FiveG_Words,"FiveG_Words.Rdata")
+
 
 # Input <- "Somebody told me to go there but I Refused to do so"
 # Input2 <- "Hello there"
 # Input3 <- "I do not work there"
 # Input4 <- "me"
-
+# Input5 <- "I went to Johns Hopkins"
 
 
 
@@ -116,16 +136,43 @@ MakeNGrams <- function(Sentence)
                   Start <- len-i
                   End <- len
                   x[i+1] <<- word(Sentence,Start,End)
-              
             }
-            
-            
-  
+
+         
+
+    Inp_1g <<-  data.table(x[1] )  
+    for(i in 2:4)
+    {
+      assign(paste("Inp_",i,"g",sep=''),data.table(str_split_fixed(x[i]," ", i)),
+                            envir = .GlobalEnv) 
+    }
 }
 
-MakeNGrams(Input)
 
-Base_FiveGram[match(x[4],word(Base_FiveGram$V1,1,4))]
-Base_FourGram[match(x[3],word(Base_FourGram$V1,1,3))]
-Base_ThreeGram[match(x[2],word(Base_ThreeGram$V1,1,2))]
-Base_TwoGram[match(x[1],word(Base_TwoGram$V1,1,1))]
+
+rm(Corp,Fivegram,Fourgram,Threegram,Tok,Train,Twogram)
+
+
+
+MakeNGrams(Input5)
+
+
+
+FiveG_Words[V1==Inp_4g[[1]] & V2==Inp_4g[[2]] &
+            V4== Inp_3g[[3]]& V2==Inp_4g[[4]] ,c(5,6) ]
+
+
+FourG_Words[V1==Inp_3g[[1]] & V2==Inp_3g[[2]] &
+              V3== Inp_3g[[3]],c(4,5) ]
+
+ThreeG_Words[V1==Inp_2g[[1]] & V2==Inp_2g[[2]],c(3,4)]
+
+TwoG_Words[V1==Inp_1g[[1]] ,c(2,3)]
+
+OneG_Words
+
+
+
+
+
+
