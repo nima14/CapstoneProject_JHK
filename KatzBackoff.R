8b0278d663b115150d014183f4a17d1e434f62de
Nima <nimataghidoost@gmail.com>
2020-05-16 18:32:25 +0430

    nls added

--- KatzBackoff.R
+++ KatzBackoff.R
@@ -2,6 +2,7 @@ library(dplyr)
 FF_OneG <- data.table( table(OneG_Words$N) )
 
 FF_TwoG <- data.table( table(TwoG_Words$N) )
+FF_TwoG$V1 <- as.numeric(FF_TwoG$V1)
 
 FF_ThreeG <- data.table( table(ThreeG_Words$N) )
 
@@ -10,23 +11,14 @@ FF_FourG <- data.table( table(FourG_Words$N) )
 FF_FiveG <- data.table( table(FiveG_Words$N) )
 
 
-Bins <- FF_FiveG
+nls1=nls(N~i*V1^-z,start=list(i=300000,z=3),data=FF_TwoG)
+FF_TwoG <-  data.table( FF_TwoG,New_N= as.integer(predict(nls1)) )
 
-Bins[2,V1]
-max <- dim(Bins)[1]
-r<-2:(max-1)
-Bins[1, Zr:=2*N/Bins[2,V1]]  # r=1, q=0, Zr=Nr/(0.5t)
-Bins[r, Zr:=2*Nr/(Bins[r+1,c]-Bins[r-1,c])]  # else, Zr=Nr/(0.5(t-q))
-Bins[max, Zr:=Nr/(c-Bins[(max-1),c])]  # r=max, t=2r-q, Zr=Nr/(r-q)
 
+newdata <- data.frame(V1=1:max(FF_TwoG$V1+1)) 
+
+pred <- data.table(newdata$V1,predict(nls1,newdata =newdata))
 
 
 
-Bins[64,2*N/(Bins[64+1,V1]-Bins[64-1,V1] )]
 
-Bins[64+1,V1]
-
-Bins[64-1,V1]
-
-
-Bins[2*Bins$N/(Bins[4+1,V1]-Bins[4-1,V1])] 
