7b39b08ede2db34480fa6d42b6bd381348dc0ff9
Nima <Nimataghidoust@gmail.com>
2020-06-05 20:36:56 +0430

    NewKatzBackoff.R was renamed to KatBackoff_Model.R

--- KatzBackoff_Model.R
+++ KatzBackoff_Model.R
@@ -7,37 +7,6 @@ library(quanteda)
 -----------------------------------------------------------
 
 
-OneG_Words   <- readRDS("OneG_Words2.Rdata")
-TwoG_Words   <- readRDS("TwoG_Words2.Rdata")
-ThreeG_Words <- readRDS("ThreeG_Words2.Rdata")
-FourG_Words  <- readRDS("FourG_Words2.Rdata")
-FiveG_Words  <- readRDS("FiveG_Words2.Rdata")
-
-
-names(OneG_Words) <- c("Pred","N")
-
-
-names(TwoG_Words) <- c("Prefix","Pred","N")
-
-
-ThreeG_Words <- data.table(paste(ThreeG_Words$V1,ThreeG_Words$V2),ThreeG_Words$V3,ThreeG_Words$N)
-names(ThreeG_Words) <- c("Prefix","Pred","N")
-
-
-FourG_Words <- data.table(paste(FourG_Words$V1,FourG_Words$V2,FourG_Words$V3),FourG_Words$V4,FourG_Words$N)
-names(FourG_Words) <- c("Prefix","Pred","N")
-
-
-FiveG_Words <- data.table(paste(FiveG_Words$V1,FiveG_Words$V2,FiveG_Words$V3,FiveG_Words$V4),FiveG_Words$V5,FiveG_Words$N)
-names(FiveG_Words) <- c("Prefix","Pred","N")
-
-
-setkey(TwoG_Words,Prefix)
-setkey(ThreeG_Words,Prefix)
-setkey(FourG_Words,Prefix)
-setkey(FiveG_Words,Prefix)
-
-
 # Input <- "Somebody told me to go there but I Refused to do so"
 # Input2 <- "Hello there"
 # Input3 <- "I do not work there"
@@ -59,86 +28,81 @@ setkey(FiveG_Words,Prefix)
 # Input19 <- "have to put on"
 
 
-
-
-Sentence <- Input18
-gamma2 <- 0.5
+gamma2 <- 2
 gamma3 <- 0.5
 gamma4 <- 0.5
 gamma5 <- 0.5
 
 
 
-MakeNGrams2 <- function(Sentence)
+MakeNGrams <- function(Sentence)
 {
   Sentence <- tolower(Sentence) 
   x <- as.character( rep("",4))
   len <-  sapply(strsplit(Sentence, " "), length)
   
-  for(i in 0:min(3,len-1))
-  {
-    Start <- len-i
-    End <- len
-    x[i+1] <- word(Sentence,Start,End)
-  }
+      for(i in 0:min(3,len-1))
+      {
+        Start <- len-i
+        End <- len
+        x[i+1] <- word(Sentence,Start,End)
+      }
   
   
-  for(i in 1:4)
-  {
-    assign(paste("Inp_",i,"g",sep=''),data.table(x[i]),
-           envir = .GlobalEnv) 
-  }
-  
+      for(i in 1:4)
+      {
+        assign(paste("Inp_",i,"g",sep=''),data.table(x[i]),
+               envir = .GlobalEnv) 
+      }
+      
   MyList <- list(Inp_1g,Inp_2g,Inp_3g,Inp_4g)
   return(MyList)
 }
 
-Sentence <- "I can not lie about what i hear"
+Sentence <- "I try"
 
 
-
-
-
-
-
-GetObsProbs <- function(Sentence,gamma2=0.5,gamma3=0.5,gamma4=0.5,gamma5=0.5){
+Ngram_Words <- readRDS("Ngram_words.Rdata")
+ #---------------------------------------------------------------------
+GetObsProbs <- function(Sentence,gamma2=2,gamma3=0.5,gamma4=0.5,gamma5=0.5){
   
-  ngrams <- MakeNGrams2(Sentence)
+  ngrams <- MakeNGrams(Sentence)
   Inp_1g <- ngrams[[1]]
   Inp_2g <- ngrams[[2]]
   Inp_3g <- ngrams[[3]]
   Inp_4g <- ngrams[[4]]
-  
+ 
   #------------------------------------------------------------               
-  obs_5 <- data.table(Ngram_Words3[Ngram_Words3$Prefix==Inp_4g[[1]] & 
-                                     Ngram_Words3$ngram==5 ,c(2,4)] %>%
-                        arrange(desc(N)))
+  obs_5 <- Ngram_Words[Ngram_Words$Prefix==Inp_4g[[1]] & 
+                                     Ngram_Words$ngram==5 ,c(2,3)] %>%
+                        arrange(desc(N)) %>% data.table()
   TotalSum_5 <- sum(obs_5$N)
   obsProb_5 <- data.table(obs_5$Pred,(obs_5$N-gamma5)/TotalSum_5)
   alpha_5 <- 1 - sum(obsProb_5$V2)
   #------------------------------------------------------------               
-  obs_4 <- data.table(Ngram_Words3[Ngram_Words3$Prefix==Inp_3g[[1]] & 
-                                     Ngram_Words3$ngram==4 ,c(2,4)] %>%
-                        arrange(desc(N)))
+  obs_4 <- Ngram_Words[Ngram_Words$Prefix==Inp_3g[[1]] & 
+                                     Ngram_Words$ngram==4 ,c(2,3)] %>%
+                        arrange(desc(N)) %>% data.table()
   TotalSum_4 <- sum(obs_4$N)
   obsProb_4 <- data.table(obs_4$Pred,(obs_4$N-gamma4)/TotalSum_4)
   alpha_4 <- 1 - sum(obsProb_4$V2)
-  #------------------------------------------------------------               
-  obs_3 <- data.table(Ngram_Words3[Ngram_Words3$Prefix==Inp_2g[[1]] & 
-                                     Ngram_Words3$ngram==3 ,c(2,4)] %>%
+  #------------------------------------------------------------  
+  obs_3 <- data.table(Ngram_Words[Ngram_Words$Prefix==Inp_2g[[1]] & 
+                                    Ngram_Words$ngram==3 ,c(2,3)] %>%
                         arrange(desc(N)))
+ 
   TotalSum_3 <- sum(obs_3$N)
   obsProb_3 <- data.table(obs_3$Pred,(obs_3$N-gamma3)/TotalSum_3)
   alpha_3 <- 1 - sum(obsProb_3$V2)
   #------------------------------------------------------------
-  obs_2 <- data.table(Ngram_Words3[Ngram_Words3$Prefix==Inp_1g[[1]] & 
-                                     Ngram_Words3$ngram==2 ,c(2,4)] %>%
+  obs_2 <- data.table(Ngram_Words[Ngram_Words$Prefix==Inp_1g[[1]] & 
+                                     Ngram_Words$ngram==2 ,c(2,3)] %>%
                         arrange(desc(N)))
   TotalSum_2 <- sum(obs_2$N)
   obsProb_2 <- data.table(obs_2$Pred,(obs_2$N-gamma2)/TotalSum_2)
   alpha_2 <- 1 - sum(obsProb_2$V2)
   #------------------------------------------------------------
-  TotalWords <- data.table(Ngram_Words3[Ngram_Words3$ngram==1,c(2,4)] %>% arrange(desc(N)))
+  TotalWords <- data.table(Ngram_Words[Ngram_Words$ngram==1,c(2,3)] %>% arrange(desc(N)))
   unobs_1 <- TotalWords[!(Pred %in% obs_2$Pred),]
   TotalSum_1 <- sum(unobs_1$N)
   Prob_1 <- data.table(unobs_1$Pred,alpha_2*unobs_1$N/TotalSum_1)
