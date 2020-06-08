a7f0c99a2ef6251d3914f1ec516cb1dc5c3a7435
Nima <nimataghidoost@gmail.com>
2020-06-08 13:49:51 +0430

    KatzBackoff_Model.R was changed. Inputs were deleted and now the result of the function is a data.table.

--- KatzBackoff_Model.R
+++ KatzBackoff_Model.R
@@ -4,75 +4,33 @@ library(stringr)
 library(data.table)
 library(quanteda)
 
------------------------------------------------------------
+#---------------------------------------------------------------------
 
-
-# Input <- "Somebody told me to go there but I Refused to do so"
-# Input2 <- "Hello there"
-# Input3 <- "I do not work there"
-# Input4 <- "me"
-# Input5 <- "I went to Johns Hopkins"
-# Input6 <- "Let's have a look at"
-# Input7 <-"The guy in front of me just bought a pound of bacon, a bouquet, and a case of"
-# Input8 <- "You're the reason why I smile everyday. Can you follow me please? It would mean the"
-# Input9 <- "Hey sunshine, can you follow me and make me the"
-# Input10 <- "Very early observations on the Bills game: Offense still struggling but the"
-# Input11 <- "Go on a romantic date at the"
-# Input12 <- "Well I'm pretty sure my granny has some old bagpipes in her garage I'll dust them off and be on my"
-# Input13 <- "Ohhhhh #PointBreak is on tomorrow. Love that film and haven't seen it in quite some"
-# Input14 <- "After the ice bucket challenge Louis will push his long wet hair out of his eyes with his little"
-# Input15 <- "Be grateful for the good times and keep the faith during the"
-# Input16 <- "If this isn't the cutest thing you've ever seen, then you must be"
-# Input17 <- "Sell the"
-# Input18 <- "This Video will be"
-# Input19 <- "have to put on"
-#Input20 <- "When you breathe, I want to be the air for you. I'll be there for you, I'd live and I'd"
-Input21 <- "Guy at my table's wife got up to go to the bathroom and I asked about dessert and he started telling me about his"
-Input22 <- "I'd give anything to see arctic monkeys this"
-Input23 <- "Talking to your mom has the same effect as a hug and helps reduce your"
-Input24 <- "When you were in Holland you were like 1 inch away from me but you hadn't time to take a"
-Input25 <- "I'd just like all of these questions answered, a presentation of evidence, and a jury to settle the"
-Input26 <- "I can't deal with unsymetrical things. I can't even hold an uneven number of bags of groceries in each"
-Input27 <- "Every inch of you is perfect from the bottom to the"
-Input28 <- "I’m thankful my childhood was filled with imagination and bruises from playing"
-Input29 <- "I like how the same people are in almost all of Adam Sandler's"
-
-
-gamma2 <- 2
-gamma3 <- 0.5
-gamma4 <- 0.5
-gamma5 <- 0.5
-
-
-
-MakeNGrams <- function(Sentence)
-{
-  Sentence <- tolower(Sentence) 
-  x <- as.character( rep("",4))
-  len <-  sapply(strsplit(Sentence, " "), length)
+  MakeNGrams <- function(Sentence)
+  {
+    Sentence <- tolower(Sentence) 
+    x <- as.character( rep("",4))
+    len <-  sapply(strsplit(Sentence, " "), length)
+    
+    for(i in 0:min(3,len-1))
+    {
+      Start <- len-i
+      End <- len
+      x[i+1] <- word(Sentence,Start,End)
+    }
+    
+    
+    for(i in 1:4)
+    {
+      assign(paste("Inp_",i,"g",sep=''),data.table(x[i]),
+             envir = .GlobalEnv) 
+    }
+    
+    MyList <- list(Inp_1g,Inp_2g,Inp_3g,Inp_4g)
+    return(MyList)
+  }
   
-      for(i in 0:min(3,len-1))
-      {
-        Start <- len-i
-        End <- len
-        x[i+1] <- word(Sentence,Start,End)
-      }
-  
-  
-      for(i in 1:4)
-      {
-        assign(paste("Inp_",i,"g",sep=''),data.table(x[i]),
-               envir = .GlobalEnv) 
-      }
-      
-  MyList <- list(Inp_1g,Inp_2g,Inp_3g,Inp_4g)
-  return(MyList)
-}
 
-Sentence <- "I try"
-
-
-Ngram_Words <- readRDS("Ngram_words.Rdata")
  #---------------------------------------------------------------------
 GetObsProbs <- function(Sentence,gamma2=2,gamma3=0.5,gamma4=0.5,gamma5=0.5){
   
@@ -130,7 +88,8 @@ GetObsProbs <- function(Sentence,gamma2=2,gamma3=0.5,gamma4=0.5,gamma5=0.5){
   Prob_4 <- data.table(Total4GNot5G$V1,alpha_5*Total4GNot5G$V2/TotalSumProb_4)
   #------------------------------------------------------------  
   Prob_5 <- rbind(obsProb_5,Prob_4)
-  top_n(Prob_5,5,V2)
+  names(Prob_5) <- c("Prediction","Probability")
+  data.table(top_n(Prob_5,5,Probability))
   
 }
 
